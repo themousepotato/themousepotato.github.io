143f71e224b0793ec95178284c897a84292491d0
Navaneeth Suresh <navaneeths1998@gmail.com>
2024-07-28 23:24:26 +0530

    chore: update website title

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: 'themousepotato',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'navaneeth-website',
+	title: 'Navaneeth Suresh',
 	// Meta property used as the default description meta property
 	description: 'Personal website of Navaneeth Suresh',
 	// HTML lang property, found in src/layouts/Base.astro L:18
