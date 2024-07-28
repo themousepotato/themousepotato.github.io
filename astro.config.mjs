4c9caf65a9a033049b5844608ef6fe19e166dc4b
Navaneeth Suresh <navaneeths1998@gmail.com>
2024-07-28 18:40:55 +0530

    chore: change render modede to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
 
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,8 +40,4 @@ export default defineConfig({
 		}
 	},
 	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
 })
